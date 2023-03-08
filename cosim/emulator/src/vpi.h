7888f57b09f48242c0f564ca4d8752ddeb337f16
Yanqi Yang <shxxyyq0000@qq.com>
2023-03-08 22:15:19 +0800

    add vpi_get_64

--- cosim/emulator/src/vpi.h
+++ cosim/emulator/src/vpi.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <verilated_vpi.h>
+#include <cstdlib>
 
 inline auto vpi_get_integer(const char *name) {
   vpiHandle handle = vpi_handle_by_name((PLI_BYTE8 *) name, nullptr);
@@ -8,4 +9,14 @@ inline auto vpi_get_integer(const char *name) {
   val.format = vpiIntVal;
   vpi_get_value(handle, &val);
   return val.value.integer;
+}
+
+inline uint64_t vpi_get_64(const char *name) {
+  vpiHandle handle = vpi_handle_by_name((PLI_BYTE8 *) name, nullptr);
+  s_vpi_value val;
+  val.format = vpiHexStrVal;
+  vpi_get_value(handle, &val);
+  const char* p = val.value.str;
+  uint64_t result = std::strtoul(p, nullptr, 16);
+  return result;
 }
